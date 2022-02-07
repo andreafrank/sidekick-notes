c630e544935ee916536ebc88c106303542499138
andreafrank <andreajfrank23@gmail.com>
2022-02-07 23:59:44 +0100

    create notes controller + associated views

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'notes/index'
+  get 'notes/new'
+  get 'notes/create'
+  get 'notes/edit'
+  get 'notes/update'
+  get 'notes/show'
+  get 'notes/destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
